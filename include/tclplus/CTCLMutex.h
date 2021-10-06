465592156017c23b4d47ad3b331b592d0c4c5f4a
Ron Fox <fox@nscl.msu.edu>
2021-10-06 11:59:50 +0000

    Add threading support to libtcl++
    Tested CTCLMutex and associated CriticalSection classes.
    Note: I was not able to get Tcl_MutexLock to actually work,
    the race condition tests failed miserably no matter what I did
    to try to the the lock to lock.  What I did instead was
    import the CMutex class from NSCLDAQ/main/base/thread which
    is known to work...and do some using's to alias CTCLMutex -> CMutex
    and CTCLMutexAttr -> CMutexAttr.
    
    This results in a much more flexible mutex class but...leaves a
    disappointing mystery I can't solve.

--- include/tclplus/CTCLMutex.h
+++ include/tclplus/CTCLMutex.h
@@ -1,73 +1,112 @@
+#ifndef CTCLMUTEX_H
+#define CTCLMUTEX_H
 /*
     This software is Copyright by the Board of Trustees of Michigan
-    State University (c) Copyright 2017.
+    State University (c) Copyright 2005.
 
     You may use this software under the terms of the GNU public license
     (GPL).  The terms of this license are described at:
 
      http://www.gnu.org/licenses/gpl.txt
 
-     Authors:
+     Author:
              Ron Fox
-             Giordano Cerriza
 	     NSCL
 	     Michigan State University
 	     East Lansing, MI 48824-1321
 */
 
-/** @file:  CTCLMutex.h
- *  @brief: Class that encpsulates a Mutex via the Tcl API.
- */
-#ifndef CTCLMUTEX_H
-#define CTCLMUTEX_H
-#include <tcl.h>
+// Note: For reasons unknown to me I can't get Tcl_MutexLock to actually
+//       do anything so my tests failed.  I do know how to use pthread mutexes
+//       so I imported this working class from NSCLDAQ/main/base/thread
+//       and then some typedefs to make names ok.
 
-/**
- *  @class CTCLMutex
- *     Class to encapsulate a mutex using the Tcl api.
- *     - Construction creates the mutex,
- *     - Destruction finalizes the mutex,
- *     - Lock and Unlock do that as well.
- *
- * @note - we don't create the mutex with TCL_DECLARE_MUTEX because
- *         *   They make it a static and that's bad for us.
- *         *   We know we need threading turned on so the other reason --
- *             making it a nullity is not relevant for us.
- */
-class CTCLMutex {
-private:
-    Tcl_Mutex m_mutex;
-public:
-    CTCLMutex();
-    virtual ~CTCLMutex();
-    
-    void lock();
-    void unlock();
+#include <pthread.h>
+
+/*!
+  Direct encapsulation of a pthread mutex attribute block.  We only support the
+attributes in the base pthreads specification (not the realtime nor the
+advanced realtime specs), as you should be able to count on those always being
+available
+
+*/
+struct CMutexAttr
+{
+  pthread_mutexattr_t  m_attributes;
+  CMutexAttr();
+  ~CMutexAttr();
+
+  void setShared();
+  void setUnshared();
+  bool isShareable();
+  
+  void setType(int type);
+  int  getType();
+
+  static void  throwifbad (int status, const char* message);
 };
 
-/**
- * @class CTCLCriticalSection
- *    This class implements a critical section of code.  Critical sections
- *    of code are sections of code that can only be executed by one thread at a time.
- *    CTCLCritical section constructs on a mutex and locks that mutex at construction
- *    on destruction the mutex is unlocked.  A typical example is:
- * \verbatim
- *      CTCLMutex guard;      // Used to guard the critical sections:
- *      ...
- *      {
- *         CTCLCriticalSection(guard);        // guard is locked
- *              ... Thread critical code
- *      }                                     // guard is unlocked.
- */
-class CTCLCriticalSection
+
+/*!
+  Direct encapsulation of a pthread  mutex.
+
+*/
+class CMutex
 {
-private:
-    CTCLMutex& m_mutex;
 public:
-    CTCLCriticalSection (CTCLMutex& mutex) : m_mutex(mutex) {
+  pthread_mutex_t      m_mutex;            //  The underlying mutex.
+  
+public:
+  CMutex();
+  CMutex(pthread_mutexattr_t& attributes);
+  CMutex(CMutexAttr&          attributes);
+  virtual ~CMutex();
+
+private:
+  CMutex(const CMutex&);
+  CMutex& operator=(const CMutex&);
+  int operator==(const CMutex&) const;
+  int operator!=(const CMutex&) const;
+
+
+
+  // Synchronization operations.
+public:
+
+  void lock();
+  bool trylock();
+  void unlock();
+
+private:
+  void create(pthread_mutexattr_t* pAttributes);
+};
+
+
+
+/**
+ *  Safe lock/unlock of mutexes
+ *  Declaring a CriticalSection object in a block will lock the mutex and
+ *  gaurantee the mutex is unlocked when the block is exited.
+ *  The scope of the block is the period the mutex is held.
+ *  
+ */
+class CriticalSection {
+private:
+    CMutex& m_mutex;
+public:
+    CriticalSection(CMutex& mutex) : m_mutex(mutex)
+    {
         m_mutex.lock();
     }
-    ~CTCLCriticalSection() {m_mutex.unlock(); }
+    ~CriticalSection() {
+        m_mutex.unlock();
+    }
 };
+// Sneaky trick.
+
+using CTCLMutex = CMutex;
+using CTCLMutexAttr = CMutexAttr;
+
+
 
 #endif
---
+++ tclplus/mutextests.cpp
@@ -0,0 +1,87 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  mutextests.cpp
+ *  @brief: Test CTCLMutex.
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include <CTCLThread.h>
+#include <CTCLMutex.h>
+#include <TCLInterpreter.h>
+
+// I really don't know a good way to test this other than
+// to run something that should create a race condition but does not.
+// since it's mutex guarded.
+
+class mutextests : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(mutextests);
+    CPPUNIT_TEST(testrace);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+    CTCLInterpreter* m_pInterp;
+    int m_counter;
+    CTCLMutex* m_pMutex;
+    
+public:
+    void setUp() {
+        m_pInterp = new CTCLInterpreter;
+        m_counter = 0;
+        m_pMutex  = new CTCLMutex;
+    }
+    void tearDown() {
+        delete m_pMutex;
+        delete m_pInterp;
+    }
+protected:
+    void testrace();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(mutextests);
+
+void mutextests::testrace()
+{
+    class MyThread : public CTCLThread
+    {
+    private:
+        CTCLMutex& m_mutex;
+        volatile int*       m_counter;
+    public:
+        MyThread(CTCLMutex* pMutex, int*  counter) :
+            m_mutex(*pMutex), m_counter(counter) {}
+        int operator()(ClientData) {
+            for(int i =0; i < 10000; i++) {
+                CriticalSection c(m_mutex);
+                (*m_counter)++;
+            }
+            return 0;
+        }
+    };
+    MyThread t1(m_pMutex, &m_counter);
+    MyThread t2(m_pMutex, &m_counter);
+    MyThread t3(m_pMutex, &m_counter);
+    MyThread t4(m_pMutex, &m_counter);
+    t1.start(); t2.start();  t3.start(); t4.start();
+    t1.join(); t2.join(); t3.join(); t4.join();
+    
+    // If the race conditions were alleviated, the result of counter should
+    // be 10000 * # threads
+    
+    EQ(40000, m_counter);
+}
